9a34e0a3de7e99bf24fe87a8e598670ee2e42466
Bruno Coulet <bruno.coulet@laplateforme.io>
2026-02-11 21:47:31 +0100

    Classe Perceptron ok

--- perceptron.py
+++ perceptron.py
@@ -1,8 +1,8 @@
 import random
 # import numpy as np
 
-
 LEARNING_RATE = 0.1
+THRESHOLD = .5
 
 # ======== Début - Données factices générées de manière aléatoire ================
 try:
@@ -12,7 +12,7 @@ except ValueError:
     exit()
 
 try:
-    THRESHOLD = float(input("Choisissez le seuil (entrez un chiffre) : "))
+    THRESHOLD = float(input("Choisissez le seuil (entrez un décimal) : "))
 except ValueError:
     print("Veuillez entrer un NOMBRE, sinon cela ne fonctionnera pas.\n")
     exit()
@@ -37,13 +37,49 @@ print(f"weights générés :\n{weights}\n")
 
 
 # ========= Perceptron en POO ====================================
-
 class Perceptron:
 
-    def __init__(self, input_size=INPUT_SIZE, weights=weights, threshold=THRESHOLD, learning_rate=LEARNING_RATE):
+    def __init__(self, input_size=INPUT_SIZE, inputs=inputs, weights=weights, threshold=THRESHOLD, learning_rate=LEARNING_RATE):
 
         self.input_size = input_size
-        self.weights = weight
+        self.inputs = inputs
+        self.weights = weights
         self.threshold = threshold
         self.learning_rate = learning_rate
         self.bias = 0.0
+
+    # Fonction d'activation
+    def threshold_function(self, weighted_sum):
+        print(f"Seuil : {self.threshold}\n")
+        print("Calcul de la fonction d'activation :")
+        if weighted_sum >= self.threshold:
+            print(f"Somme pondérée : {weighted_sum}\n{weighted_sum} >= threshold\n")
+            return 1
+        else:
+            print(f"Somme pondérée : {weighted_sum}\n{weighted_sum} < threshold\n")
+            return 0
+
+
+    # Fonction de calcul du perceptron (appelle la fonction d'activation)
+    def perceptron(self):
+
+        weighted_sum=0
+        print(f"\nLongueur du vecteur d'input : ({len(inputs)},)\n")
+
+        print("Calcul de la somme pondérée :")
+        for x,w in zip(inputs, weights):
+            weighted_sum += x * w
+        print(f"Somme pondérée = {weighted_sum}\n")
+
+        return self.threshold_function(weighted_sum)
+
+
+
+
+
+if __name__ == "__main__" :
+
+    output = Perceptron()
+    # print(f"Output = {str(output)}\n")
+    sortie = output.perceptron()
+    print(f"Sortie du perceptron = {str(sortie)}\n")
