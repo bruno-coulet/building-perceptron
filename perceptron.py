e19e2a1eff08a9c69a0bcbd5268a931ee953f65c
Bruno Coulet <bruno.coulet@laplateforme.io>
2026-02-11 14:28:02 +0100

    perceptron en programmation séquentielle

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,52 @@
+# Implémentation d'un perceptron en programmation séquentielle
+
+x_input = [2, 5, 1, 8]
+w_weights = [.4, .3, .2, .1]
+threshold = .5
+
+
+
+# Fonction d'activation : step function
+def threshold_step(weighted_sum):
+    print("Calcul de la fonction d'activation :")
+    if weighted_sum >= threshold:
+        print(f"Threshold : {threshold}\nSomme pondérée : {weighted_sum}\nSomme pondérée >= threshold\n")
+        return 1
+    else:
+        print(f"Threshold : {threshold}\nSomme pondérée : {weighted_sum}\nSomme pondérée < threshold\n")
+        return 0
+
+
+
+# Fonction de calcul du perceptron (appelle la fonction d'activation)
+def perceptron():
+
+    weighted_sum=0
+    print(f"\nVecteur d'input : ({len(x_input)},)\n")
+    print("Calcul de la somme pondérée :")
+
+    for x,w in zip(x_input, w_weights):
+        weighted_sum += x * w
+        print(f"Somme partielle index {x_input.index(x)} : {x} * {w} = {str(weighted_sum)}")
+    print()
+
+    return threshold_step(weighted_sum)
+
+output = perceptron()
+print(f"Output = {str(output)}\n")
+
+
+
+
+
 # Implémentation d'un perceptron en POO
 
 class Perceptron:
-    pass
+
+    def __init__(self, input_size, weight, threshold, learning_rate):
+
+        self.input_size = input_size
+        self.weights = weight
+        self.threshold = threshold
+        self.learning_rate = learning_rate
+        self.bias = 0.0
