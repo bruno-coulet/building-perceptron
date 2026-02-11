3bd4481ffa796471d6cb3bffc551e1060b6df35a
Bruno Coulet <bruno.coulet@laplateforme.io>
2026-02-11 18:45:01 +0100

    debut de POO

--- perceptron.py
+++ perceptron.py
@@ -2,8 +2,9 @@ import random
 # import numpy as np
 
 
+LEARNING_RATE = 0.1
 
-# ======== Données factices générées de manière aléatoire ================
+# ======== Début - Données factices générées de manière aléatoire ================
 try:
     INPUT_SIZE = int(input("\nChoisissez la taille de l'input (entrez un chiffre) : "))
 except ValueError:
@@ -16,26 +17,30 @@ except ValueError:
     print("Veuillez entrer un NOMBRE, sinon cela ne fonctionnera pas.\n")
     exit()
 
-LEARNING_RATE = 0.1
-
-
-x_input = []
-w_weights = []
+inputs = []
+weights = []
 
 for _ in range(INPUT_SIZE):
     # génère un entier, bornes inclusives [0, 10]
-    x_input.append(random.randint(0, 10))
+    inputs.append(random.randint(0, 10))
     # génère un float,distribution uniforme sur l'intervalle [0.0, 1.0[
-    w_weights.append(random.random())
+    weights.append(random.random())
 
-print(f"\ninput générés :\n{x_input}\n")
-print(f"weights générés :\n{w_weights}\n")
+print(f"\ninput générés :\n{inputs}\n")
+print(f"weights générés :\n{weights}\n")
+
+# ======== FIN - Données factices générées de manière aléatoire ================
+
+
+
+
+
 
 # ========= Perceptron en POO ====================================
 
 class Perceptron:
 
-    def __init__(self, input_size, weight, threshold, learning_rate):
+    def __init__(self, input_size=INPUT_SIZE, weights=weights, threshold=THRESHOLD, learning_rate=LEARNING_RATE):
 
         self.input_size = input_size
         self.weights = weight
